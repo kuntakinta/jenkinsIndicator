c53785155419273c371c52b6ecb05ba3ad020a2c
J.Palka <j.palka@smartrecruiters.com>
2016-10-19 15:57:54 +0200

    Update GroovyScript.groovy

--- src/main/groovy/GroovyScript.groovy
+++ src/main/groovy/GroovyScript.groovy
@@ -8,8 +8,8 @@ def success=true
 //list of all jobs taken into consideration in certain job
 def jobNames = build.buildVariableResolver.resolve("JOBS").split(',')
 
-for( String value : jobNames ){
-    println("Checking job: " + value)
+for( String jobName : jobNames ){
+    println("Checking job: " + jobName)
     Job job = Hudson.instance.getJob(value)
 
     if (job.lastCompletedBuild.result != Result.SUCCESS){

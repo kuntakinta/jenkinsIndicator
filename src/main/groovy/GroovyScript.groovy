84e6199c88200e0cf7cbedec367f12ab3b7deba6
J.Palka <j.palka@smartrecruiters.com>
2016-10-19 17:04:01 +0200

    Update GroovyScript.groovy

--- src/main/groovy/GroovyScript.groovy
+++ src/main/groovy/GroovyScript.groovy
@@ -4,23 +4,22 @@ import hudson.model.Result
 import hudson.AbortException
 
 
-def success=true
+def success = true
 //list of all jobs taken into consideration in certain job
 def jobNames = build.buildVariableResolver.resolve("JOBS").split(',')
 
-for( String jobName : jobNames ){
-    println("Checking job: " + jobName)
-    Job job = Hudson.instance.getJob(jobName)
+for (String value : jobNames) {
+    println("Checking job: " + value)
+    Job job = Hudson.instance.getJob(value)
 
-    if (job.lastCompletedBuild.result != Result.SUCCESS){
-        println("job " + jobName + " was not successful, link to job result page: " + job.lastCompletedBuild.getAbsoluteUrl())
-        success=false
+    if (job.lastCompletedBuild.result != Result.SUCCESS) {
+        println("job " + value + " has failed, link to job result page: " + job.lastCompletedBuild.getAbsoluteUrl())
+        success = false
     }
 }
 
-if (success != true ) {
+if (success != true) {
     throw new AbortException("ERROR - FAILURE")
-}
-else{
+} else {
     println "SUCCESS"
 }
